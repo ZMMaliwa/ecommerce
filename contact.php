5c8c4635776570590c3ba71085a2cf404dd876e5
ZWMaliwa <maliwazukisa@gmail.com>
2023-01-11 16:15:46 +0200

    online-store

---
+++ contact.php
@@ -0,0 +1,57 @@
+<?php include 'inc/header.php'; ?>
+
+ <div class="main">
+    <div class="content">
+    	<div class="support">
+  			<div class="support_desc">
+  				<h3>Live Support</h3>
+  				<p><span>24 hours | 7 days a week | 365 days a year &nbsp;&nbsp; Maliwa Industries</span></p>
+  				<p>This is a newly found international online selling company that aims to uplift the ecommerce industry. its' director Zukisa Maliwa is a CodeSpace graduate</p>
+  			</div>
+  				<img src="web/images/contact.png" alt="" />
+  			<div class="clear"></div>
+  		</div>
+    	<div class="section group">
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="SUBMIT"></span>
+						  </div>
+					    </form>
+				  </div>
+  				</div>
+				<div class="col span_1_of_3">
+      			<div class="company_address">
+				     	<h2>Company Information :</h2>
+						    	<p>25 Symphoney way,</p>
+						   		<p>Bellville 7535</p>
+								<p>Cape Town</p>
+						   		<p>South Arica</p>
+				   		<p>Phone:(073) 135 5418</p>
+				   		<p>Fax: (078) 440 1258</p>
+				 	 	<p>Email: <span>info@MaliwaIndustries.com</span></p>
+				   		<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
+				   </div>
+				 </div>
+			  </div>    	
+    </div>
+ </div>
+<?php include 'inc/footer.php'; ?>
